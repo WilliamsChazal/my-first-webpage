49c5edbfc4ea1d3c4a20d998052cdb99a82f1a72
williams <w.chazal@codeur.online>
2021-03-16 16:33:16 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>La page web de Williams</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <h1 id="title">La page Web de Lord Williams</h1>  
+</body>
+</html>
